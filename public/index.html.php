91a448f85eb04f471cb94884bd81b4d8b78216dd
Mihajlo Andjelic <mihajlo88@gmail.com>
2015-05-10 13:06:35 +0200

    staticke stvari

--- public/index.html.php
+++ public/index.html.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<script src="public/js/script.js"></script>
+	<link rel="stylesheet" href="public/css/style.css">
 	<meta charset="UTF-8">
 	<title>Nesto</title>
 </head>
 <body>
 	nesto no 2
+
+	<div class="la">lkasjdlkjasdkljl</div>
 </body>
 </html>
